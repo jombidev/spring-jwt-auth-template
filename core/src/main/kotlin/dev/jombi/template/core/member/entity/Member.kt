f6e4663b5503c54366f51a61924e231515d98bb9
jombi <jombi@duck.com>
2024-11-07 20:59:46 +0900

    fix :: IdWrapper

--- core/src/main/kotlin/dev/jombi/template/core/member/entity/Member.kt
+++ core/src/main/kotlin/dev/jombi/template/core/member/entity/Member.kt
@@ -1,17 +1,22 @@
 package dev.jombi.template.core.member.entity
 
-import dev.jombi.template.core.common.entity.BaseIdTimeEntity
+import dev.jombi.template.core.common.entity.BaseTimeEntity
+import dev.jombi.template.core.common.id.WrappedLong
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 
 @Entity(name = "tb_member")
 data class Member(
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     val credential: String,
 
-    @Column
+    @Column(nullable = false)
     val password: String, // bcrypt
 
-    @Column
+    @Column(nullable = false)
     val name: String,
-) : BaseIdTimeEntity()
+
+    @Id
+    val id: WrappedLong = WrappedLong.NULL
+) : BaseTimeEntity()
