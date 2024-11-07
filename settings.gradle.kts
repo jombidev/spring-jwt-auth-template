38d3aebab30d9ac90481b3b30f43c8cf7fa1fb80
jombi <jombi@duck.com>
2024-11-07 20:59:21 +0900

    chore :: naming fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "spring_templates"
+rootProject.name = "spring-jwt-auth-template"
 include("api")
 include("business")
 include("common")
